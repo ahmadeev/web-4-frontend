a7b51c131eace2e21029f90f00194094b87a4db1
danis <ahrnadeev@gmail.com>
2024-12-05 22:36:46 +0300

    Add checkboxes

--- src/components/CreateShot.jsx
+++ src/components/CreateShot.jsx
@@ -1,18 +1,101 @@
+import {useState} from "react";
 
 function CreateShot() {
+    const [x, setX] = useState("");
+    const [y, setY] = useState("");
+    const [r, setR] = useState("");
+
+    const values = [
+        "-2.0", "-1.5", "-1.0",
+        "-0.5", "0.0", "0.5",
+        "1.0", "1.5", "2.0"
+    ]
+
+    const [checkboxes, setCheckboxes] = useState(
+        values.reduce((acc, value) => ({ ...acc, [value]: false }), {})
+    );
+
+    const handleXChange = (e) => {
+        setX(e.target.value);
+    }
+
+    const handleYChange = (e) => {
+        setY(e.target.value);
+    }
+
+    const handleRChange = (e) => {
+        setR(e.target.value);
+    }
+
+    const handleCheckboxChange = (event, setCheckboxes) => {
+        const { name, checked } = event.target;
+        setCheckboxes((prev) => ({
+            ...prev,
+            [name]: checked,
+        }));
+    };
+
+    const handleCheckboxRequest = () => {
+        const result = []
+        for(const [k, v] of Object.entries(checkboxes)) {
+            if (v === true) {
+                result.push(Number(k));
+            }
+        }
+        console.log(result);
+        return result;
+    }
 
     return (
         <div>
             <form>
-                <h2>Информация о драконе</h2>
+                <h2>ФОРМА ДЛЯ ТОЧКИ</h2>
                 <div className="form-section">
                     <div className="form-group">
                         <label>x:</label>
-                        <input type="text"/><br/>
+                        <input
+                            value={x}
+                            onChange={(e) => {
+                                handleXChange(e)
+                            }}
+                            type="text"
+                        /><br/>
                         <label>y:</label>
-                        <input type="text"/><br/>
+                        <input
+                            value={y}
+                            onChange={(e) => {
+                                handleYChange(e)
+                            }}
+                            type="text"
+                            placeholder="from -5 to 3"
+                        /><br/>
                         <label>r:</label>
-                        <input type="text"/><br/>
+                        <input
+                            value={r}
+                            onChange={(e) => {
+                                handleRChange(e)
+                            }}
+                            type="text"
+                        /><br/>
+
+                        {values && values.map((value, index) => (
+                            <>
+                                <label key={index}>
+                                    <input
+                                        type="checkbox"
+                                        name={value}
+                                        checked={checkboxes[value]}
+                                        onChange={handleCheckboxChange}
+                                    />
+                                    {value}
+                                </label>
+                                {(index + 1) % 3 === 0 && <br/>}
+                            </>
+                        ))}
+
+                        {handleCheckboxRequest()}
+
+
                     </div>
                 </div>
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,6 +6,17 @@ import {crudCreate, crudRead, crudUpdate, crudDelete, crudReadMany, crudDeleteMa
 import ShotTable from "../components/ShotTable.jsx";
 import CreateShot from "../components/CreateShot.jsx";
 import Alert from "../components/Alert/Alert.jsx";
+import {
+    R,
+    CENTER,
+    draw_graph,
+    form_path_string,
+    form_polygon_string, X_HALF_R,
+    X_MINUS_HALF_R,
+    X_MINUS_R,
+    Y_HALF_R,
+    Y_MINUS_R
+} from "../utils/drawGraph.js";
 
 function Home({ pageTitle }) {
 
@@ -21,9 +32,41 @@ function Home({ pageTitle }) {
 
     useEffect(() => {
         document.title = pageTitle;
-    })
+
+        const svg = document.querySelector("svg");
+
+        let polygon_points = {
+            2: form_polygon_string([
+                `${CENTER}, ${CENTER}`,
+                `${X_MINUS_R}, ${CENTER}`,
+                `${CENTER}, ${Y_HALF_R}`
+            ]),
+            3: form_polygon_string([
+                `${CENTER}, ${CENTER}`,
+                `${CENTER}, ${Y_MINUS_R}`,
+                `${X_MINUS_HALF_R}, ${Y_MINUS_R}`,
+                `${X_MINUS_HALF_R}, ${CENTER}`,
+            ]),
+        }
+
+        let path_points = {
+            1: form_path_string({
+                "L"     : `${X_HALF_R}, ${CENTER}`,
+                "A"     : `${R / 2}, ${R / 2}`,
+                "ANGLE" : "0",
+                "END"   : `${CENTER}, ${Y_HALF_R}`
+            }),
+        }
+
+        draw_graph(svg, polygon_points, path_points)
+    }, [])
 
     const BASE_URL = "http://localhost:8080/backend-jakarta-ee-1.0-SNAPSHOT/api/user";
+
+    const svgStyle = {
+        height: "250px",
+        width: "250px",
+    }
 
     return (
         <>
@@ -32,6 +75,17 @@ function Home({ pageTitle }) {
                 <h2>
                     ПРОВЕРКА ПОПАДАНИЯ В ОБЛАСТЬ
                 </h2>
+
+                <div className="main_content">
+                    <div className={styles.content_left}>
+                        <h2>ГРАФИК</h2>
+                        <svg style={svgStyle}></svg>
+                    </div>
+                    <div className={styles.content_right}>
+                        <CreateShot/>
+                    </div>
+                </div>
+
 
                 <button onClick={() => setCreateShotModalActive(true)}>CREATE POPUP</button>
                 <button onClick={() => setModalActive(true)}>Открыть модальное окно</button>
