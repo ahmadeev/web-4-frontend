fae77808758b0dca2157d80b6642e534e7ecc88f
danis <ahrnadeev@gmail.com>
2024-12-11 20:10:18 +0300

    Add negative R handling function

--- src/utils/graph.js
+++ src/utils/graph.js
@@ -1,7 +1,13 @@
 
 export const drawDots = (r, shots) => {
+    if (!r) {
+        console.log("нет r");
+        return;
+    }
+
     console.log("начали рисовать")
     console.log("shots:", shots)
+    console.log("r:", r)
     for (let shot of shots) {
         console.log("shot:", shot)
         drawDot(shot.x, shot.y, shot.r, shot.hit, r)
@@ -14,10 +20,13 @@ export const drawDot = (x, y, r, isHit, lastR) => {
     const CENTER_CONST = svg.getBoundingClientRect().height / 2;
     const R_CONST = 80
 
-    x += R_CONST * x / r + CENTER_CONST
-    y += -R_CONST * y / r + CENTER_CONST
+    // x += R_CONST * x / r + CENTER_CONST
+    // y += -R_CONST * y / r + CENTER_CONST
 
-    console.log(`x: ${x}, y: ${y}, r: ${r}, isHit: ${isHit}`);
+    x += R_CONST * x / lastR + CENTER_CONST
+    y += -R_CONST * y / lastR + CENTER_CONST
+
+    // console.log(`x: ${x}, y: ${y}, r: ${r}, isHit: ${isHit}`);
 
     const dot = document.createElementNS('http://www.w3.org/2000/svg', 'circle')
     dot.setAttributeNS(null, 'cx', x);
@@ -26,7 +35,7 @@ export const drawDot = (x, y, r, isHit, lastR) => {
     dot.setAttributeNS(null, 'r', '3');
 
     let dotColor;
-    console.log(`r: ${r}, last r: ${lastR}`);
+    // console.log(`r: ${r}, last r: ${lastR}`);
     if (r === parseFloat(lastR)) {
         dotColor = (isHit ? 'fill: green; stroke: black;' : 'fill: red; stroke: black;')
     } else {
@@ -35,5 +44,5 @@ export const drawDot = (x, y, r, isHit, lastR) => {
 
     dot.setAttributeNS(null, 'style', dotColor);
     svg.appendChild(dot);
-    console.log("точка нарисована")
+    // console.log("точка нарисована")
 }
